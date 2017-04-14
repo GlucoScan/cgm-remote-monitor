c4701c2947d56606c779d37a9fbe37ed3313f453
GlucoScan <controlbetter@gmail.com>
2017-04-14 23:12:55 +0100

    Update bundle.source.js

--- bundle/bundle.source.js
+++ bundle/bundle.source.js
@@ -6,16 +6,16 @@
   window.moment = require('moment-timezone');
   window.sugar = require('sugar');
   window.crossfilter = require('crossfilter');
-  window.Nightscout = window.Nightscout || {};
+  window.GlucoScan = window.GlucoScan || {};
 
-  window.Nightscout = {
+  window.GlucoScan = {
     client: require('../lib/client')
     , units: require('../lib/units')()
     , report_plugins: require('../lib/report_plugins/')()
     , admin_plugins: require('../lib/admin_plugins/')()
   };
 
-  console.info('Nightscout bundle ready');
+  console.info('GlucoScan bundle ready');
 
 })();
 
